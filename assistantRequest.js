88f4be8cf954e71784670ccd3212a889ab5b0a31
Matthieu Court <matthieu.court@gmail.com>
2018-01-24 21:30:05 -0500

    testing things out

---
+++ assistantRequest.js
@@ -0,0 +1,19 @@
+"use strict";
+// This is a shim for the request object from express (https://github.com/expressjs/express/blob/master/lib/request.js) because the
+// actions-on-google module assumes it is working with an express request object.
+function assistantRequest(lambda_event) {
+  console.log("lambda_event");
+  //console.log(lambda_event);
+  var AuthToken = lambda_event.headers.AuthToken;
+  console.log("****Security Token: " + AuthToken);
+  this.body = JSON.parse(lambda_event.body);
+  if (AuthToken != 'L045i3vezQG2C6R') {
+    setBreakpoint();
+  }
+}
+
+assistantRequest.prototype.get = function get(field) {
+  return null;
+};
+
+module.exports = assistantRequest;
---
+++ index.js
@@ -0,0 +1,65 @@
+/* jshint node: true */
+/*jshint esversion: 6 */
+
+"use strict";
+
+var AssistantRequest = require('./assistantRequest');
+var AssistantResponse = require('./assistantResponse');
+
+var Sync = require('synchronize');
+var assistantResponse = new AssistantResponse();
+
+const LOCATION_PERMISSION_ACTION = 'locationPermissionAction';
+
+
+function returnLambdaResponse(assistantResponse, context) {
+  // lambda_response is the object to return that API Gateway understands
+  var lambda_response = {
+    "statusCode": assistantResponse.statusCode,
+    "headers": {
+      "Content-Type": "application/json",
+      "Google-Assistant-API-Version": "v2"
+    },
+    "body": JSON.stringify(assistantResponse.body)
+  };
+  context.succeed(lambda_response);
+}
+
+function ask(app, inputPrompt) {
+  app.data.lastPrompt = inputPrompt;
+  app.ask(inputPrompt);
+}
+
+function askForConfirmation(app, inputPrompt) {
+  app.data.lastPrompt = inputPrompt;
+  app.askForConfirmation(inputPrompt);
+}
+
+
+function testAction(app) {
+  app.ask("this is a test");
+}
+
+
+exports.handler = function(event, context, callback) {
+  process.env.DEBUG = 'actions-on-google:*';
+  var assistantRequest = new AssistantRequest(event);
+  //var ActionsSdkAssistant = require('actions-on-google').ApiAiAssistant;
+  var ActionsSdkAssistant = require('actions-on-google').DialogflowApp;
+
+  var assistant = new ActionsSdkAssistant({
+    request: assistantRequest,
+    response: assistantResponse
+  });
+
+  const actionMap = new Map();
+
+    // actionMap.set("providerSearchAction", providerSearchIntent);
+    // actionMap.set(SEND_MESSAGE_ACTION, sendMessageAction);
+    actionMap.set("testAction", testAction)
+
+    Sync.fiber(function() {
+    assistant.handleRequest(actionMap);
+    returnLambdaResponse(assistantResponse, context);
+  });
+};
